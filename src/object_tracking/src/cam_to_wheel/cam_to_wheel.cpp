e0397020f241ab06b5195e28a20bdd1758315204
Chartreuse Sylvex <a1085125@mail.nuk.edu.tw>
2021-11-07 15:10:54 +0800

    added pid and launch file to modify the pid param in real time, default feature may added later

--- src/object_tracking/src/cam_to_wheel/cam_to_wheel.cpp
+++ src/object_tracking/src/cam_to_wheel/cam_to_wheel.cpp
@@ -5,7 +5,15 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "Cam_to_Wheel");
     ros::NodeHandle nh;
 
-    Cam_to_Wheel cam_to_wheel(nh);
+    double kp, ki, kd;
+
+    nh.getParam("/cam_to_wheel_node/kp", kp);
+    nh.getParam("/cam_to_wheel_node/ki", ki);
+    nh.getParam("/cam_to_wheel_node/kd", kd);
+
+    PID pid(kp, ki, kd, 0.0);
+
+    Cam_to_Wheel cam_to_wheel(nh,pid);
 
     cam_to_wheel.Recieve();
 
